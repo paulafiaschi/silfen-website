30699e43b8646ff1ad23b8ccb7df0f6dae75b6dc
Paula Fiaschi <paul6399@stud.kea.dk>
2021-11-17 11:34:51 +0100

    burger menu js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+window.addEventListener("load", startmain);
 
+function startmain() {
+  console.log("start main page");
+  document.querySelector(".burger-icon").addEventListener("click", openMenu);
+  document.querySelector(".cross-icon").addEventListener("click", closeMenu);
+}
+
+function openMenu() {
+  console.log("open menu");
+  document.querySelector(".mob-menu").classList.remove("close");
+  document.querySelector(".mob-menu").classList.add("show");
+  document.querySelector(".burger-icon").removeEventListener("click", openMenu);
+  document.querySelector(".cross-icon").addEventListener("click", closeMenu);
+}
+function closeMenu() {
+  console.log("close menu");
+  document.querySelector(".mob-menu").classList.remove("show");
+  document.querySelector(".mob-menu").classList.add("close");
+  document.querySelector(".cross-icon").removeEventListener("click", closeMenu);
+  document.querySelector(".burger-icon").addEventListener("click", openMenu);
+}
